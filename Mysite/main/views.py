9f41f17151414c16250e25c37b383e0b183324b6
lucho03 <l.valentinov.ivanov@gmail.com>
2021-12-07 22:11:50 +0200

    Error in the function

--- Mysite/main/views.py
+++ Mysite/main/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+class View(TemplateView):
+    def dashboard(request):
+        return render(request, 'dashboard.html')
