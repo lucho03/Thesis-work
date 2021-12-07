9e519e3bc6208153ade881aec5f3336908ec0e9b
lucho03 <l.valentinov.ivanov@gmail.com>
2021-12-07 22:20:38 +0200

    Add file with url paths in my app

---
+++ Mysite/main/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+from main.views import View
+
+urlpatterns = [
+    path('', View.dashboard),
+    path('dashboard', View.dashboard)
+]
