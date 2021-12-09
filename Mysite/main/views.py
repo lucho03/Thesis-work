56ff837bc2fc0119adaad1f8742ced065a44d452
lucho03 <l.valentinov.ivanov@gmail.com>
2021-12-09 23:29:10 +0200

    Change view file

--- Mysite/main/views.py
+++ Mysite/main/views.py
@@ -4,3 +4,6 @@ from django.views.generic.base import TemplateView
 class View(TemplateView):
     def dashboard(request):
         return render(request, 'dashboard.html')
+
+    def about_us(request):
+        return render(request, 'about_us.html')
