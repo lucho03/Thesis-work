2ff03b4e72be9c9a38ca0f93ce2f465b0f9f3240
lucho03 <l.valentinov.ivanov@gmail.com>
2021-12-10 22:03:26 +0200

    Set '' path to be for main page and tickets.html in paths

--- Mysite/main/views.py
+++ Mysite/main/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 class View(TemplateView):
+    def main_page(request):
+        value = 'HelpDesk System'
+        return render(request, 'main_page.html', {'value': value})
+    
     def dashboard(request):
         return render(request, 'dashboard.html')
 
     def about_us(request):
         return render(request, 'about_us.html')
+
+
+class Tickets(TemplateView):
+    def set_ticket():
+        pass
+
+    def tickets(request):
+        return render(request, 'tickets.html')
