5406a1d31940c38fd22e6686d3fccdd08d1f58f9
lucho03 <l.valentinov.ivanov@gmail.com>
2021-12-14 22:49:12 +0200

    Create model of ticket

--- Mysite/main/models.py
+++ Mysite/main/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
-class Ticket(models.Model):
+class TicketModel(models.Model):
     author = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     text = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return 'Author : ' + self.author + ' Email: ' + self.email + ' Text: ' + self.text
