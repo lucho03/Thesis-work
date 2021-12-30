078b21847569c96d6d4a90a48237365f9490571f
lucho03 <l.valentinov.ivanov@gmail.com>
2021-12-30 12:06:20 +0200

    Adding new paths to register, log_in and logout_user views

--- Mysite/main/urls.py
+++ Mysite/main/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth import logout
 from django.urls import path
 
 from main.views import Tickets, View
@@ -8,5 +9,8 @@ urlpatterns = [
     path('dashboard', View.dashboard),
     path('about us', View.about_us),
     path('tickets', Tickets.get_tickets),
-    path('send_ticket', Tickets.set_ticket)
+    path('send_ticket', Tickets.set_ticket),
+    path('register', View.register, name='register'),
+    path('logout_user', View.logout_user),
+    path('log_in', View.log_in)
 ]
