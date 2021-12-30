bf01713d1b9f1d31bc23d147f9af8ae3e206e159
lucho03 <l.valentinov.ivanov@gmail.com>
2021-12-30 12:13:31 +0200

    Adding register, log_in and logout_user functions. Register create new user and set him in the system. Log_in check if user exists and after that he can login. Logout_user logging out the user

--- Mysite/main/views.py
+++ Mysite/main/views.py
@@ -1,14 +1,52 @@
 from django import forms
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.contrib.auth.models import User
 
-from .forms import TicketModelForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from .forms import TicketModelForm, UserForm
 from .models import TicketModel 
+
+from django.contrib import messages
 
 class View(TemplateView):
     def main_page(request):
-        value = 'HelpDesk System'
-        return render(request, 'main_page.html', {'value': value})
+        return render(request, 'main_page.html')
+    
+    def register(request):
+        if request.method == 'POST':
+            form = UserForm(request.POST)
+            if form.is_valid():
+                user = form.save()
+                login(request, user)
+                return HttpResponseRedirect('/dashboard')
+            else:
+                return HttpResponse(request.POST)
+        form = UserForm()
+        return render(request, 'login_user.html', {'form': form})
+    
+    def logout_user(request):
+        logout(request)
+        return HttpResponseRedirect('/')
+    
+    def log_in(request):
+        if request.method == 'POST':
+            form = AuthenticationForm(request, data=request.POST)
+            if form.is_valid:
+                username = form.data['username']
+                password = form.data['password']
+                user = authenticate(username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    return HttpResponseRedirect('/dashboard')
+                else:
+                    messages.error(request, 'Wrong username or password!')
+        
+        form = AuthenticationForm()
+        return render(request, 'login_user.html', {'form': form})
     
     def dashboard(request):
         return render(request, 'dashboard.html')
@@ -18,15 +56,19 @@ class View(TemplateView):
 
 
 class Tickets(TemplateView):
+    @login_required(login_url='/log_in')
     def set_ticket(request):
         form = TicketModelForm(request.POST)
         if request.method == 'POST':    
             if form.is_valid():
-                form.save()
+                curr = form.save(commit=False)
+                curr.author = request.user
+                curr.save()
                 return render(request, 'send_ticket.html', {'form':TicketModelForm(request.GET)})
             
         return render(request, 'send_ticket.html', {'form':form})
-
+    
+    @login_required(login_url='/log_in')
     def get_tickets(request):
-        tickets = TicketModel.objects.all
+        tickets = TicketModel.objects.all().filter(author=request.user)
         return render(request, 'tickets.html', {'tickets':tickets})
