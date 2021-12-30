d39e43ffefa60d9e355f6dbca818aa9060f1bfec
lucho03 <l.valentinov.ivanov@gmail.com>
2021-12-30 11:52:34 +0200

    Optimizing of the ticket model. Add foreign key to User model and remove email field and author like charfiled

--- Mysite/main/models.py
+++ Mysite/main/models.py
@@ -1,10 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.timezone import now
 
 class TicketModel(models.Model):
-    author = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     text = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return 'Author : ' + self.author + ' Email: ' + self.email + ' Text: ' + self.text
+    #date = models.DateTimeField(default=now, blank=True)
