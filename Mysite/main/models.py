627b8928a25712874bbd7d18ec8369d2b0f9bf5f
lucho03 <l.valentinov.ivanov@gmail.com>
2022-01-20 21:13:22 +0200

    Adding date and permission to the ticket model

--- Mysite/main/models.py
+++ Mysite/main/models.py
@@ -1,8 +1,17 @@
+import email
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils.timezone import now
 
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+
 class TicketModel(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
-    #date = models.DateTimeField(default=now, blank=True)
+    date = models.DateTimeField(default=now, blank=True)
+
+    class Meta:
+        permissions = (
+            ('answer_tickets', 'can answer tickets'),
+        )
