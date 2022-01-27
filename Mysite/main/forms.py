e861a7e0081661aec9fbbdfbc2d8b69a71ec02bf
lucho03 <l.valentinov.ivanov@gmail.com>
2022-01-27 21:59:41 +0200

    Adding new form for AnswerModel

--- Mysite/main/forms.py
+++ Mysite/main/forms.py
@@ -1,6 +1,7 @@
+from dataclasses import field
 from django import forms
-from django.forms import Textarea
-from .models import TicketModel
+from django.forms import Textarea, widgets
+from .models import AnswerModel, TicketModel
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -17,12 +18,26 @@ class TicketModelForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(TicketModelForm, self).__init__(*args, **kwargs)
         self.fields['text'].required = False
+        self.fields['priority'].required = False
+        self.fields['type'].required = False
     
     class Meta:
         model = TicketModel
-        fields = ['text']
+        fields = ['text', 'priority', 'type']
         widgets = {
             'text': Textarea(
                     {'placeholder':('What do you want to know?')}
                 )
+        }
+
+class AnswerModelForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(AnswerModelForm, self).__init__(*args, **kwargs)
+        self.fields['text'].required = False
+    
+    class Meta:
+        model = AnswerModel
+        fields = ['text']
+        widgets = {
+            'text': Textarea()
         }
