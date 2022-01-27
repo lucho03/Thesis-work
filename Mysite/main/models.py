eba807013ed5c86267c12da170674977c2ae46a6
lucho03 <l.valentinov.ivanov@gmail.com>
2022-01-27 21:58:51 +0200

    Adding priority, status and kind of tickets. Adding also new model - AnswerModel which is solution for tikcet. One tikcet can have more than one answer so the connection is one to many

--- Mysite/main/models.py
+++ Mysite/main/models.py
@@ -1,12 +1,34 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.utils.timezone import now, localtime
-import datetime
 
 class TicketModel(models.Model):
+    PRIORITIES = (
+        ('1', 'Urgent'),
+        ('2', 'High'),
+        ('3', 'Medium'),
+        ('4', 'Low')
+    )
+    TYPES = (
+        ('Q', 'Question'),
+        ('P', 'Problem'),
+        ('I', 'Incident'),
+        ('R', 'Request'),
+        ('O', 'Order')
+    )
+    STATUSES = (
+        ('O', 'Open'),
+        ('P', 'Pending'),
+        ('R', 'Resolved'),
+        ('C', 'Closed')
+    )
+
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    priority = models.CharField(max_length=1, choices=PRIORITIES, default='4')
+    type = models.CharField(max_length=1, choices=TYPES, default='Q')
+    status = models.CharField(max_length=1, choices=STATUSES, default='O')
+    num_answers = models.IntegerField(default=0)
 
     class Meta:
         permissions = (
@@ -14,3 +36,8 @@ class TicketModel(models.Model):
             ('rewrite_tickets', 'can change tickets'),
             ('answer_tickets', 'can answer tickets')
         )
+
+class AnswerModel(models.Model):
+    ticket = models.ForeignKey(TicketModel, on_delete=models.CASCADE)
+    text = models.TextField()
+    number = models.IntegerField(default=0)
