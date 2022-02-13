5bd9f5d754d862078c5a563a3d0457d5372b7c95
lucho03 <l.valentinov.ivanov@gmail.com>
2022-02-13 20:22:26 +0200

    Change

--- Mysite/main/urls.py
+++ Mysite/main/urls.py
@@ -7,6 +7,7 @@ from main.views import Tickets, View
 urlpatterns = [
     path('', View.main_page),
     path('dashboard', View.dashboard),
+    path('profile', View.profile),
     path('about us', View.about_us),
     path('tickets', Tickets.get_tickets),
     path('send_ticket', Tickets.set_ticket),
