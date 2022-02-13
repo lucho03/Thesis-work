80988f554861dcdb7493008af5f98c855aa852d2
lucho03 <l.valentinov.ivanov@gmail.com>
2022-02-13 20:43:44 +0200

    Change

--- Mysite/main/migrations/0024_auto_20220210_1558.py
+++ Mysite/main/migrations/0024_auto_20220210_1558.py
@@ -11,10 +11,6 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RemoveField(
-            model_name='answermodel',
-            name='comments',
-        ),
-        migrations.RemoveField(
             model_name='ticketmodel',
             name='comments',
         ),
