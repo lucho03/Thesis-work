e8b11a18c2a807a412aa02b853958f79d08b37fd
lucho03 <l.valentinov.ivanov@gmail.com>
2022-03-07 21:45:35 +0200

    Serializing the comments too

--- Mysite/api/urls.py
+++ Mysite/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .view import answers, tickets, data
+from .view import answers, tickets, comments_answer, comments_ticket, data
 
 urlpatterns = [
     path('', data, name='api_data'),
     path('tickets', tickets, name='api_tickets'),
-    path('answers', answers, name='api_answers')
+    path('answers', answers, name='api_answers'),
+    path('comments_answer', comments_answer, name='api_comments_1'),
+    path('comments_ticket', comments_ticket, name='api_comments_2')
 ]
