7b3de016cc8904a73ab9998d9d9a8f91f67ea2f8
lucho03 <l.valentinov.ivanov@gmail.com>
2022-03-07 21:45:31 +0200

    Serializing the comments too

--- Mysite/api/serializer.py
+++ Mysite/api/serializer.py
@@ -1,5 +1,6 @@
+from attr import field
 from rest_framework.serializers import ModelSerializer
-from main.models import TicketModel, AnswerModel
+from main.models import TicketModel, AnswerModel, CommentAnswerModel, CommentTicketModel
 
 class TicketSerializer(ModelSerializer):
     class Meta:
@@ -10,3 +11,13 @@ class AnswerSerializer(ModelSerializer):
     class Meta:
         model = AnswerModel
         fields = ['id', 'ticket', 'text', 'answer_comments', 'number']
+
+class CommentAnswerSerializer(ModelSerializer):
+    class Meta:
+        model = CommentAnswerModel
+        fields = ['answer', 'text', 'number']
+
+class CommentTicketSerializer(ModelSerializer):
+    class Meta:
+        model = CommentTicketModel
+        fields = ['ticket', 'text', 'number']
