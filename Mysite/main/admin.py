ed502fcc37e2c47376f1ba7336dc1c391a7a07d2
lucho03 <l.valentinov.ivanov@gmail.com>
2022-03-11 12:57:31 +0200

    Trying something with agents

--- Mysite/main/admin.py
+++ Mysite/main/admin.py
@@ -1,8 +1,17 @@
-import imp
 from django.contrib import admin
-from .models import TicketModel, AnswerModel, CommentAnswerModel, CommentTicketModel
+from django.contrib.auth.models import User
+from .models import TicketModel, AnswerModel, CommentAnswerModel, CommentTicketModel, RegisterAgent
+from .forms import AgentForm
 
 admin.register(TicketModel)(admin.ModelAdmin)
 admin.register(AnswerModel)(admin.ModelAdmin)
 admin.register(CommentAnswerModel)(admin.ModelAdmin)
 admin.register(CommentTicketModel)(admin.ModelAdmin)
+
+class AdminRegisterAgent(admin.ModelAdmin):
+    fields = ('email',)
+    #def get_form(self, request, obj=None, **kwargs):
+    #    print(request.user)
+    
+
+admin.site.register(RegisterAgent, AdminRegisterAgent)
