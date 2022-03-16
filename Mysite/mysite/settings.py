63ee81a0049b7aaa8a215acbe33d7e45cea3ad99
lucho03 <l.valentinov.ivanov@gmail.com>
2022-03-17 00:05:07 +0200

    Checking if starting a server or testing and setting the database

--- Mysite/mysite/settings.py
+++ Mysite/mysite/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+from ast import arg
 from email.policy import default
 import os
+import sys
 import django_heroku
 import environ
 from pathlib import Path
@@ -66,7 +68,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'main/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'main/template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -85,16 +87,24 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'helpdesk',
-        'USER': 'help',
-        'PASSWORD': '18523',
-        'HOST': 'localhost',
-        'PORT': '5432',
+if sys.argv[1:2] == ['test']:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'helpdesk',
+            'USER': 'help',
+            'PASSWORD': '18523',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 
 # Password validation
