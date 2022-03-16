a36cc8aa3cff1737904d13f9aa49cc824f1a6562
lucho03 <l.valentinov.ivanov@gmail.com>
2022-03-17 00:05:43 +0200

    Trying something with admin panel

--- Mysite/main/admin.py
+++ Mysite/main/admin.py
@@ -3,15 +3,23 @@ from django.contrib.auth.models import User
 from .models import TicketModel, AnswerModel, CommentAnswerModel, CommentTicketModel, RegisterAgent
 from .forms import AgentForm
 
+'''
 admin.register(TicketModel)(admin.ModelAdmin)
 admin.register(AnswerModel)(admin.ModelAdmin)
 admin.register(CommentAnswerModel)(admin.ModelAdmin)
 admin.register(CommentTicketModel)(admin.ModelAdmin)
+'''
 
 class AdminRegisterAgent(admin.ModelAdmin):
+    list_display = ('email', )
     fields = ('email',)
-    #def get_form(self, request, obj=None, **kwargs):
-    #    print(request.user)
+    change_list_template = 'admin/change_list.html'
+    #add_list_template = 'admin/base_site.html'
     
 
 admin.site.register(RegisterAgent, AdminRegisterAgent)
+
+admin.site.register(TicketModel, admin.ModelAdmin)
+admin.site.register(AnswerModel, admin.ModelAdmin)
+admin.site.register(CommentTicketModel, admin.ModelAdmin)
+admin.site.register(CommentAnswerModel, admin.ModelAdmin)
