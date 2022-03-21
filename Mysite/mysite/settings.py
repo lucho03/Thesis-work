324821adde9e0eed89d5cc02b99921519996035a
lucho03 <l.valentinov.ivanov@gmail.com>
2022-03-21 23:36:23 +0200

    Change

--- Mysite/mysite/settings.py
+++ Mysite/mysite/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'main/template')],
+        'DIRS': [os.path.join(BASE_DIR, 'main/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
