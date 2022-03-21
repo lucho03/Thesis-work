2df70e1f8fee36010652922db806ad5ceced0e61
lucho03 <l.valentinov.ivanov@gmail.com>
2022-03-21 23:32:46 +0200

    Clearing the admin file

--- Mysite/main/admin.py
+++ Mysite/main/admin.py
@@ -1,23 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from .models import TicketModel, AnswerModel, CommentAnswerModel, CommentTicketModel, RegisterAgent
-from .forms import AgentForm
-
-'''
-admin.register(TicketModel)(admin.ModelAdmin)
-admin.register(AnswerModel)(admin.ModelAdmin)
-admin.register(CommentAnswerModel)(admin.ModelAdmin)
-admin.register(CommentTicketModel)(admin.ModelAdmin)
-'''
-
-class AdminRegisterAgent(admin.ModelAdmin):
-    list_display = ('email', )
-    fields = ('email',)
-    change_list_template = 'admin/change_list.html'
-    #add_list_template = 'admin/base_site.html'
-    
-
-admin.site.register(RegisterAgent, AdminRegisterAgent)
+from .models import TicketModel, AnswerModel, CommentAnswerModel, CommentTicketModel
 
 admin.site.register(TicketModel, admin.ModelAdmin)
 admin.site.register(AnswerModel, admin.ModelAdmin)
