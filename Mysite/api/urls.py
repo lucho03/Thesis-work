d39fd39fce08715acd2272df69557807d94c00a5
lucho03 <l.valentinov.ivanov@gmail.com>
2022-03-21 23:27:41 +0200

    New path to invitation api function

--- Mysite/api/urls.py
+++ Mysite/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .view import answers, tickets, comments_answer, comments_ticket, data
+from .view import answers, tickets, comments_answer, comments_ticket, data, invitation
 
 urlpatterns = [
     path('', data, name='api_data'),
     path('tickets', tickets, name='api_tickets'),
     path('answers', answers, name='api_answers'),
     path('comments_answer', comments_answer, name='api_comments_1'),
-    path('comments_ticket', comments_ticket, name='api_comments_2')
+    path('comments_ticket', comments_ticket, name='api_comments_2'),
+    path('invitation', invitation, name='invitation')
 ]
