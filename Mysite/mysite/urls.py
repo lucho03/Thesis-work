999a34ee5e4a3aa85270f802a37bf08443e543f8
lucho03 <l.valentinov.ivanov@gmail.com>
2022-05-09 22:01:24 +0300

    Changing the url of api

--- Mysite/mysite/urls.py
+++ Mysite/mysite/urls.py
@@ -21,5 +21,5 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('api/', include('api.urls'))
+    path('api/v1/', include('api.urls')) 
 ]
